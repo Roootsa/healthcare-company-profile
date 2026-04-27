eaeaa10a5cf8713f4661374a3816143a7df19793
wekave <151003222+wekave@users.noreply.github.com>
2026-04-27 19:43:38 +0700

    Refactor ContactPage to use ContactForm component
    Removed input and button for user interaction and replaced with ContactForm component.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,28 +1,13 @@
-"use client";
-
-import { useState } from "react";
-import ServerInfo from "../komponen/ServerInfo";
+import ContactForm from "../components/ContactForm";
+import ServerInfo from "../components/ServerInfo";
 
 export default function ContactPage() {
-  const [name, setName] = useState("");
-
   return (
     <main>
       <h1>Contact Us</h1>
 
-      {/* Server Component */}
       <ServerInfo />
-
-      {/* Client Interaksi */}
-      <input
-        type="text"
-        placeholder="Masukkan nama"
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <button onClick={() => alert("Halo " + name)}>
-        Klik Saya
-      </button>
+      <ContactForm />
     </main>
   );
 }
